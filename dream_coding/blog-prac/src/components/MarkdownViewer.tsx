82c6c5fd9d12855d111cb13274a5ca3c1df48c89
jisooround <9utesleben@gmail.com>
2023-04-20 20:38:16 +0900

    Feat: 마크다운 syntax highlighter 설정

--- dream_coding/blog-prac/src/components/MarkdownViewer.tsx
+++ dream_coding/blog-prac/src/components/MarkdownViewer.tsx
@@ -1,11 +1,36 @@
+"use client";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 type Props = {};
 
 const MarkdownViewer = ({ content }: { content: string }) => {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose lg:prose-xl"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={materialDark}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
