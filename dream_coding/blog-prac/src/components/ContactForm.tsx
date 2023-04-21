58ed96ca8af6ad0762f1061800a6f406343eeaa4
jisooround <9utesleben@gmail.com>
2023-04-21 21:11:52 +0900

    Feat: 메일 api 만들기

--- dream_coding/blog-prac/src/components/ContactForm.tsx
+++ dream_coding/blog-prac/src/components/ContactForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { sendContactEmail } from "@/service/contact";
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import Banner, { BannerData } from "./Banner";
 
@@ -9,12 +10,14 @@ type Form = {
   message: string;
 };
 
+const DEFAULT_DATE = {
+  from: "",
+  subject: "",
+  message: "",
+};
+
 const ContactForm = (props: Props) => {
-  const [form, setForm] = useState<Form>({
-    from: "",
-    subject: "",
-    message: "",
-  });
+  const [form, setForm] = useState<Form>(DEFAULT_DATE);
   const [banner, setBanner] = useState<BannerData | null>(null);
   const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
@@ -23,10 +26,25 @@ const ContactForm = (props: Props) => {
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // 페이지가 다시 로딩되지 않도록 함
     console.log(form);
-    setBanner({ message: "성공했어!", state: "success" });
-    // setTimeout(() => {
-    //   setBanner(null);
-    // }, 3000);
+    sendContactEmail(form)
+      .then(() => {
+        setBanner({
+          message: "메일을 성공적으로 보냈습니다.",
+          state: "success",
+        });
+        setForm(DEFAULT_DATE);
+      })
+      .catch(() => {
+        setBanner({
+          message: "메일 전송에 실패했습니다. 다시 시도해 주세요.",
+          state: "error",
+        });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setBanner(null);
+        }, 3000);
+      });
   };
   return (
     <section className="w-full max-w-md">
